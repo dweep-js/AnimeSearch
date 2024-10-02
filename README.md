199a635d0d5da2cb74fc95914f5e6492fe6cf921
dweep-js <musashim580@gmail.com>
2024-10-02 09:57:55 +0530

    first commit

--- README.md
+++ README.md
@@ -39,3 +39,4 @@ You can preview the production build with `npm run preview`.
 # AnimeSearch
 # AnimeSearch
 # AnimeSearch
+# AnimeSearch
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	base: '/AnimeSearch',
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
